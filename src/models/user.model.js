3f726022613fc8556a02346b68d071f45185bfbf
Vikash Tantuway <Vtboy8990@gmail.com>
2025-04-23 13:55:17 +0530

    update changes in every files

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -2,7 +2,46 @@ import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-const userSchema = new Schema({})
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            require: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+            index: true
+        },
+        email: {
+            type: String,
+            require: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        username: {
+            type: String,
+            require: true,
+            trim: true,
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        password: {
+            trype: String,
+            required: [true, 'Password is required'],
+        },
+        
+    },
+    {
+        timestamps: true
+    }
+)
 
 
 export const User = mongoose.model("User", userSchema)
